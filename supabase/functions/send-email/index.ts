b87f30ed284fbe9211d5caf9de118f474c4fc4f0
Omkar Shinde <omkartsshinde@gmail.com>
2025-08-17 23:47:05 +0530

    fix: update CORS headers for send-email edge function (add apikey)

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -4,7 +4,7 @@
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, x-client-info",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, x-client-info, apikey",
 };
 
 Deno.serve(async (req) => {
